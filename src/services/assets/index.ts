7345445bc3ab99b4d14d76fb999d035f69da0708
LeandroSaveri <leandrosaveri1106@gmail.com>
2026-03-22 17:09:33 -0300

    Add files via upload

---
+++ src/services/assets/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Assets Services Index
+ * Export all asset-related services
+ */
+
+export { assetService, AssetService, AssetCache } from './AssetService';
+export { modelLoader, ModelLoader, ModelLoadOptions, LoadedModel } from './ModelLoader';
+export { textureLoader, TextureLoader, PBRTextureSet, TextureLoadOptions } from './TextureLoader';
